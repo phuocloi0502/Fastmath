b4776b199e02a8d504e10e7517b56cfe7b642d4f
phuocloi0502 <56552476+phuocloi0502@users.noreply.github.com>
2020-05-23 10:08:46 +0700

    giao dien

---
+++ app/src/main/java/com/example/fastmath/overactivity.java
@@ -0,0 +1,14 @@
+package com.example.fastmath;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class overactivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_overactivity);
+    }
+}
